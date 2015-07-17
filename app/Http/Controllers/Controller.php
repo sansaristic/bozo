b0c0ad1d3d0acf6e7783571f21f3a6617927962a
Nouwatin Jacob <jaysansa@yahoo.com>
2015-07-17 15:57:42 +0100

    first commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+//test
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
